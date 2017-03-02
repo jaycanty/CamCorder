8f41e8a4f4c0a0205259f81700d224d11018ea19
jay <jay@jays-MBP.hsd1.nj.comcast.net>
2017-03-02 14:30:38 -0500

    Start VideoCaptureVC

--- CamCorder/VideoCaptureViewController.swift
+++ CamCorder/VideoCaptureViewController.swift
@@ -7,29 +7,104 @@
 //
 
 import UIKit
+import Foundation
+import CoreMedia
+import AVFoundation
+
+fileprivate enum CaptureState {
+    case stopped
+    case recording
+}
 
 class VideoCaptureViewController: UIViewController {
+    
+    @IBOutlet weak var controlButton: UIButton!
+    
+    let captureSession = AVCaptureSession()
+    var videoInputDevice: AVCaptureDeviceInput?
+    var movieFileOutput: AVCaptureMovieFileOutput!
+    var videoDevice: AVCaptureDevice!
+    
+    private var state: CaptureState = .stopped
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Add video input
+        videoDevice = AVCaptureDevice.defaultDevice(
+            withDeviceType: AVCaptureDeviceType.builtInWideAngleCamera,
+            mediaType: AVMediaTypeVideo,
+            position: .front
+        )
+        do {
+            videoInputDevice = try AVCaptureDeviceInput(device: videoDevice)
+            if captureSession.canAddInput(videoInputDevice) {
+                captureSession.addInput(videoInputDevice)
+            }
+        } catch {
+            print(error)
+        }
+        
+        // Add audio input
+        let audioCaptureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
+        do {
+            let audioInput = try AVCaptureDeviceInput(device: audioCaptureDevice)
+            if captureSession.canAddInput(audioInput) {
+                captureSession.addInput(audioInput)
+            }
+        } catch {
+            print(error)
+        }
+        
+        // Add output
+        movieFileOutput = AVCaptureMovieFileOutput()
+        if captureSession.canAddOutput(movieFileOutput) {
+            captureSession.addOutput(movieFileOutput)
+            // Add connection
+            let captureConnection = movieFileOutput.connection(withMediaType: AVMediaTypeVideo)!
+            if captureSession.canAdd(captureConnection) {
+                
+                if captureConnection.isVideoStabilizationSupported {
+                    captureConnection.preferredVideoStabilizationMode = .auto
+                }
+            }
+        }
+        
+        // Add capture preview layer
+        if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+            previewLayer.frame = view.bounds
+            view.layer.addSublayer(previewLayer)
+            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            let orientation: AVCaptureVideoOrientation =  AVCaptureVideoOrientation(rawValue: UIDevice.current.orientation.rawValue)!
+            previewLayer.connection.videoOrientation = orientation
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func controlButtonHit(_ sender: UIButton) {
+        switch state {
+        case .stopped:
+            sender.isSelected = true
+            captureSession.startRunning()
+            state = .recording
+        case .recording:
+            sender.isSelected = false
+            captureSession.stopRunning()
+            state = .stopped
+        }
     }
-    */
+}
 
+extension VideoCaptureViewController: AVCaptureFileOutputRecordingDelegate {
+    
+    func capture(_ captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAt fileURL: URL!, fromConnections connections: [Any]!) {
+        
+        // TODO: maybe set an observer on the file
+        print("Did start capture: \(fileURL.absoluteString)")
+    }
+    
+    func capture(_ captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAt outputFileURL: URL!, fromConnections connections: [Any]!, error: Error!) {
+        
+        print("Did end capture: \(outputFileURL.absoluteString)")
+    }
 }

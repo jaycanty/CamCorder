21533f4d3fa9a9f29e34d54a97d00db68c02533e
jay <jay@jays-MBP.hsd1.nj.comcast.net>
2017-03-02 20:30:08 -0500

    Write new url to db

--- CamCorder/FileService.swift
+++ CamCorder/FileService.swift
@@ -18,26 +18,34 @@ protocol FileServiceDelegate: class {
 class FileService {
     
     let storage = FIRStorage.storage()
+    let database = FIRDatabase.database()
     weak var delegate: FileServiceDelegate?
     
     func uploadFile(withURL url: URL) {
-
         let ref = storage.reference().child("movie.mov")
         let task = ref.putFile(url)
-        
         task.observe(.progress) { snapshot in
             if let complete = snapshot.progress?.completedUnitCount,
             let total = snapshot.progress?.totalUnitCount, total > 0 {
                 self.delegate?.update(progress: Float(complete)/Float(total))
             }
         }
-        
         task.observe(.success) { snapshot in
             self.delegate?.uploadComplete(success: true)
+            if let url = snapshot.metadata?.downloadURL() {
+                DispatchQueue.global().async {
+                    self.write(url: url)
+                }
+            }
         }
-        
         task.observe(.failure) { snapshot in
             self.delegate?.uploadComplete(success: false)
         }
+    }
+    
+    func write(url: URL) {
+        let ref = database.reference().child("videos")
+        let movieRef = ref.childByAutoId()
+        movieRef.setValue(url.absoluteString)
     }
 }
